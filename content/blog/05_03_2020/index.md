f0973c517304846a62892a2cbe6ebd42461af09b
Sam Lowe <samantha.k.lowe@gmail.com>
2020-05-03 08:59:51 -0400

    removed google anaylitics

---
+++ content/blog/05_03_2020/index.md
@@ -0,0 +1,13 @@
+---
+title: Google Analaytics Gave Me Problems
+date: "2020-05-03"
+description: "Attempted to use Google Anaylitics but twitter flagged my site"
+---
+
+I just wanted to get some data on my website so I used the Gatsby Plugin for Google Analytics.
+I had the link to my blog posted to my twitter profile. It had worked for a while, but today
+twitter had flagged my site with a warning about malicious activity.
+
+I THINK it was because I added google analytics, and I didn't add the pop up that Europe requires that says I'm collecting data.
+Not 100 percent on that. But I removed the google anayltics plug in, in hopes that will clear it up. 
+I'm still learning how to add the privacy stuff. 
--- src/components/bio.js
+++ src/components/bio.js
@@ -6,37 +6,18 @@
  */
 
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
-import Image from "gatsby-image"
+import logoPhoto from "../logo/logo.png"
+
 
 const Bio = () => {
-  const data = useStaticQuery(graphql`
-    query BioQuery {
-      samantha: file(absolutePath: { regex: "/logo.png/" }) {
-        childImageSharp {
-          fixed(width: 267) {
-            base64
-            tracedSVG
-            aspectRatio
-            width
-            height
-            src
-            srcSet
-            srcWebp
-            srcSetWebp
-            originalName
-          }
-        }
-      }
-    }
-  `)
+
 
   return (
     <div>
       <Link to="/">
         {" "}
-        <Image fixed={data.samantha.childImageSharp.fixed} alt="logo" />
+        <img src={logoPhoto} alt="Samantha Lowe" />
       </Link>
     </div>
   )
